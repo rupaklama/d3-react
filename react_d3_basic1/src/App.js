032b9a736f85ab2e9676f71b65107ff4052999ce
pak <rupaklamadev@gmail.com>
2021-08-26 13:50:23 -0400

    initial commit for react d3

---
+++ react_d3_basic1/src/App.js
@@ -0,0 +1,11 @@
+import ChartWrapper from './ChartWrapper';
+
+function App() {
+  return (
+    <div>
+      <ChartWrapper />
+    </div>
+  );
+}
+
+export default App;
---
+++ react_d3_basic1/src/D3Chart.js
@@ -0,0 +1,12 @@
+// This is a file with D3 code where we are expecting to create a Class - 'D3Chart'
+
+export default class D3Chart {
+  // The constructor() method is a special method for creating and initializing objects created within a class
+  // First thing we need to do is to add a CONSTRUCTOR method of this class
+  // It is executed automatically when a new object is created & it is used to initialize object properties.
+
+  // Here we are passing a html element - div as an initial properties as arguments in the constructor
+  constructor(element) {
+    console.log('Hello World!');
+  }
+}
