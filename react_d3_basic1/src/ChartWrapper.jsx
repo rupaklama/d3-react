86736c21021e8d8556ded1cb11a08fe713d428df
pak <rupaklamadev@gmail.com>
2021-08-27 11:02:47 -0400

    added d3 library

--- react_d3_basic1/src/ChartWrapper.jsx
+++ react_d3_basic1/src/ChartWrapper.jsx
@@ -5,15 +5,15 @@ import D3Chart from './D3Chart';
 
 const ChartWrapper = () => {
   const chartArea = useRef(null);
-  const [chart] = useState(null);
+  // const [chart] = useState(null);
 
   useEffect(() => {
     // creating an instance of D3Chart &
-    // passing 'div' element here which lets us attach our D3 elements onto this div in our D3Chart.js file
-    if (!chart) new D3Chart(chartArea.current);
-  }, [chart]);
+    // passing 'div' element here which lets us attach our D3 elements onto this div from our D3Chart.js file
+    new D3Chart(chartArea.current);
+  }, []);
 
-  return <div ref={chartArea}>Chart Wrapper</div>;
+  return <div ref={chartArea}></div>;
 };
 
 export default ChartWrapper;
--- react_d3_basic1/src/D3Chart.js
+++ react_d3_basic1/src/D3Chart.js
@@ -1,5 +1,8 @@
 // This is a file with D3 code where we are expecting to create a Class - 'D3Chart'
 
+// d3 library - importing all the modules
+import * as d3 from 'd3';
+
 export default class D3Chart {
   // The constructor() method is a special method for creating and initializing objects created within a class
   // First thing we need to do is to add a CONSTRUCTOR method of this class
@@ -7,6 +10,23 @@ export default class D3Chart {
 
   // Here we are passing a html element - div as an initial properties as arguments in the constructor
   constructor(element) {
-    console.log('Hello World!');
+    // by convention, we store SVG canvas in a variable - svg
+    // so that we can modify our SVG canvas
+    const svg = d3
+      .select(element)
+      // here we are saying we want to add SVG canvas to this element
+      .append('svg')
+      // overriding default width & height with attr method
+      .attr('width', 500)
+      .attr('height', 500);
+
+    // adding rectangle with its attribs to show up in the screen
+    svg
+      .append('rect')
+      .attr('x', 50)
+      .attr('y', 50)
+      .attr('width', 100)
+      .attr('height', 400)
+      .attr('fill', 'grey');
   }
 }
